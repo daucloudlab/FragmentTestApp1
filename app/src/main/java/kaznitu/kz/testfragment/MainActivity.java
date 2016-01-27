2f3203013012efd60970d72976936451b7a6f50a
daulet <daucloudlab@gmail.com>
2016-01-27 16:21:59 +0600

    Fragment Application Test version2

--- app/src/main/java/kaznitu/kz/testfragment/MainActivity.java
+++ app/src/main/java/kaznitu/kz/testfragment/MainActivity.java
@@ -1,27 +1,44 @@
 package kaznitu.kz.testfragment;
 
 import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import kaznitu.kz.fragments.Fragment1;
 import kaznitu.kz.fragments.Fragment2;
 
 public class MainActivity extends AppCompatActivity implements Fragment1.OnSelectedButtonListener {
-
+    private boolean mIsDynamic ;
     @Override
     public void onButtonSelected(int buttonIndex) {
         FragmentManager fm = getFragmentManager() ;
 
         Fragment2 fragment2 = (Fragment2)fm.findFragmentById(R.id.fragment2) ;
 
-        if(fragment2 != null)
+        if(fragment2 == null || !fragment2.isVisible()){
+            Intent intent = new Intent(getApplicationContext(), SecondActivity.class) ;
+            intent.putExtra("buttonIndex", buttonIndex) ;
+            startActivity(intent);
+        }else {
             fragment2.setDescription(buttonIndex);
+        }
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getFragmentManager() ;
+        Fragment1 fragment11 = (Fragment1)fm.findFragmentById(R.id.fragment1) ;
+        mIsDynamic = fragment11 == null || !fragment11.isInLayout() ;
+        if(mIsDynamic){
+            FragmentTransaction ft = fm.beginTransaction() ;
+            ft.add(R.id.container, new Fragment1(), "fragment1") ;
+            ft.commit() ;
+        }
     }
 }
--- app/src/main/java/kaznitu/kz/testfragment/SecondActivity.java
+++ app/src/main/java/kaznitu/kz/testfragment/SecondActivity.java
@@ -1,7 +1,30 @@
 package kaznitu.kz.testfragment;
 
-/**
- * Created by daulet on 1/27/16.
- */
-public class SecondActivity {
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.drawable.GradientDrawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import kaznitu.kz.fragments.Fragment2;
+
+public class SecondActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent() ;
+        int buttonIndex = intent.getIntExtra("buttonIndex", -1) ;
+        if(buttonIndex != -1){
+            FragmentManager fm = getFragmentManager() ;
+            Fragment2 fragment2 = (Fragment2)fm.findFragmentById(R.id.fragment2) ;
+            fragment2.setDescription(buttonIndex);
+        }
+
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+            finish();
+        }
+    }
 }
